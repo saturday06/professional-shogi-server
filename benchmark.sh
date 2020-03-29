670e7e0c43e1af559c7f56e6b5e78d86aa4bb562
Isamu Mogi <doyoubi@pixiv.com>
2020-03-29 19:11:02 +0900

    wip

--- benchmark.sh
+++ benchmark.sh
@@ -4,4 +4,4 @@ set -eux
 
 cd "$(dirname "$0")"
 mkdir -p target
-exec ./misc/benchmark-main.sh "$@" | tee target/benchmark-$(LC_ALL=C date +%Y%m%d%H%M%S).txt
+exec ./misc/benchmark-main.sh "$@" 2>&1 | tee target/benchmark-$(LC_ALL=C date +%Y%m%d%H%M%S).txt

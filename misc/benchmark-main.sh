803d090679bbba9ae09eda7eb2cbd24441d35616
Isamu Mogi <isamu@leafytree.jp>
2020-03-30 01:09:08 +0900

    wip

--- misc/benchmark-main.sh
+++ misc/benchmark-main.sh
@@ -7,7 +7,7 @@ run_task() (
   shift
 
   if [ $(uname -s) = "Linux" ]; then
-    cpu_list=$(awk 'BEGIN{for (i = 0; i < ARGV[1] / 4; i++) { if (i > 0) printf(","); printf("%d", i + ARGV[1] * ARGV[2] / 4);}}' $(nproc) $group)
+    cpu_list=$(awk 'BEGIN{for (i = 0; i < ARGV[1] / 2; i++) { if (i > 0) printf(","); printf("%d", i + ARGV[1] * ARGV[2] / 2);}}' $(nproc) $group)
     exec taskset --cpu-list $cpu_list "$@"
   else
     exec "$@"
@@ -22,22 +22,16 @@ run() (
   port=13001
   upstream_addr=127.0.0.1:18888
 
-  run_task 1 $executable $port $upstream_addr &
+  run_task 0 $executable $port $upstream_addr &
   executable_pid=$!
-  sleep 1
-
-  run_task 2 nginx -p "$PWD" -c "$PWD/nginx.conf" &
-  nginx_pid=$!
   sleep 3
 
-  run_task 3 wrk -d 60 -t $thread -c $connection -H 'Host: example' http://127.0.0.1:$port/$path > /dev/null
+  run_task 1 wrk -d 60 -t $thread -c $connection -H 'Host: example' http://127.0.0.1:$port/$path > /dev/null
   sleep 1
-  run_task 3 wrk -d 120 -t $thread -c $connection -H 'Host: example' http://127.0.0.1:$port/$path
+  run_task 1 wrk -d 120 -t $thread -c $connection -H 'Host: example' http://127.0.0.1:$port/$path
 
   kill $executable_pid
   wait $executable_pid || true
-  kill $nginx_pid
-  wait $nginx_pid || true
 
   sleep 10
 )
@@ -63,8 +57,9 @@ fi
 ./generate-data.sh
 cargo build --release
 (cd driver/go && go build)
+run_task 1 nginx -p "$PWD" -c "$PWD/nginx.conf" &
 
-for thread in $(expr $(nproc) / 4); do
+for thread in $(expr $(nproc) / 2); do
   for path in 8k 16k 32k 64k 128k 256k 512k 768k 1m 2m 3m 4m 6m 8m 16m 32m 64m; do
     for connection in 100; do
       for executable in $(find ./driver -name "*.sh" | sort); do

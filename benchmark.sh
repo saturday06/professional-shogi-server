99eecedcfb917ed8e4208eacbd209dd46047bd65
Isamu Mogi <isamu@leafytree.jp>
2020-04-06 20:00:12 +0900

    wip

--- benchmark.sh
+++ benchmark.sh
@@ -4,4 +4,6 @@ set -eux
 
 cd "$(dirname "$0")"
 mkdir -p target
-exec ./misc/benchmark-main.sh "$@" 2>&1 | tee target/benchmark-$(LC_ALL=C date +%Y%m%d%H%M%S).txt
+start_time=$(LC_ALL=C date +%Y%m%d%H%M%S)
+./misc/benchmark-main.sh "$@" 2>&1 | tee target/benchmark-${start_time}.txt || true
+cat target/benchmark-${start_time}.txt | grep -E 'run \./driver/|Requests/sec:|Trnsfer/sec:|Socket errors: connect'| tee target/summary-${start_time}.txt

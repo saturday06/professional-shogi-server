61df3af4aef80eac8ab9d7d68ae18950df42ca16
Isamu Mogi <isamu@leafytree.jp>
2020-04-16 23:55:22 +0900

    wip

--- misc/benchmark-main.sh
+++ misc/benchmark-main.sh
@@ -89,9 +89,9 @@ WARNING
   sleep 3
 
   for iteration in 1 2; do
-    for thread in $(expr $(nproc) / 4); do
+    for thread in $(expr $(nproc) / $num_groups); do
       for path in 8k 16k 32k 64k 128k 256k 512k 768k 1m 2m 3m 4m 6m 8m 16m 32m 64m; do
-        for connection in $(expr $(nproc) \* 4); do
+        for connection in $(expr $(nproc) / $num_groups \* 16); do
           for executable in $(find ./driver -name "*.sh" | sort); do
             run $num_groups $wrk_group $benchmark_group $executable $path $connection $thread
           done
